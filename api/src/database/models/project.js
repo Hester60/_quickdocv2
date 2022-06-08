5db2d7c18003b4dc60de586baf0fe67e7e38b5c1
Hester60 <thomascarbuccia@hotmail.fr>
2022-06-08 18:33:12 +0200

    Catching mongoose validation errors

--- api/src/database/models/project.js
+++ api/src/database/models/project.js
@@ -3,16 +3,9 @@ const { Schema, model } = require('mongoose');
 const projectSchema = new Schema({
     name: {
         type: String,
-        unique: true,
         required: [true, 'Project name is required'],
         maxlength: [30, "Project name cannot exceed 30 characters"],
         set: value => value.trim(),
-        validate: {
-            validator: function(value) {
-                return value !== "";
-            },
-            message: "Project name cannot be empty"
-        }
     }
 });
 
