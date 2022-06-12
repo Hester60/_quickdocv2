978734f40bce2083059bfa11dcfd103e199a3146
Hester60 <thomascarbuccia@hotmail.fr>
2022-06-12 23:44:35 +0200

    Removing app

--- app/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: `http://${window.location.hostname}:${process.env.REACT_APP_API_PORT}${process.env.REACT_APP_API_BASE_PATH}/`,
-    timeout: 3000
-});
-
-export default api;
