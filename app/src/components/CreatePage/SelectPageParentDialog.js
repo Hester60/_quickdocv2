d5875ce2071a7d9ee5ce89ef1b39597e8c872be9
Hester60 <thomascarbuccia@hotmail.fr>
2022-06-15 00:04:16 +0200

    Editing select parent dialog

--- app/src/components/CreatePage/SelectPageParentDialog.js
+++ app/src/components/CreatePage/SelectPageParentDialog.js
@@ -6,34 +6,40 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import {useEffect, useState} from "react";
 import Button from '@mui/material/Button';
-import {Autocomplete} from '@mui/material';
+import {Autocomplete, createFilterOptions} from '@mui/material';
 import api from '../../api';
 import {useSelector} from "react-redux";
 
 export default function SelectPageParentDialog() {
     const [open, setOpen] = useState(true);
-    const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [pages, setPages] = useState([]);
     const currentProject = useSelector(state => state.currentProject.item);
+    const OPTIONS_LIMIT = 10;
+    const ROOT_SELECTION = {_id: '*', title: 'Project Root (No Parent)'};
+    const [selectedPage, setSelectedPage] = useState(ROOT_SELECTION);
 
+    // TODO : Don't fetch children !! (API EVOLUTION)
     useEffect(() => {
         (async () => {
-            await refreshPages();
+            setIsLoading(true);
+            const res = await api.get(`pages?project=${currentProject._id}&projection=_id,title`);
+            setPages([ROOT_SELECTION, ...res.data.pages]);
+            setIsLoading(false);
         })();
     }, []);
 
-    // TODO Trottle
-    async function onInputChange(e, value) {
-        await refreshPages(value);
-    }
+    const filterOptions = createFilterOptions({
+        limit: OPTIONS_LIMIT
+    });
 
-    async function refreshPages(title = '') {
-        setIsLoading(true);
-        const res = await api.get(`pages?project=${currentProject._id}&projection=_id,title&limit=7&q=${title}`);
-        setPages(res.data.pages);
-        setIsLoading(false);
+    const onSelectionChange = (e, val, reason) => {
+        if (reason === 'clear') {
+            setSelectedPage(ROOT_SELECTION);
+        } else {
+            setSelectedPage(val);
+        }
     }
 
     return (
@@ -43,28 +49,31 @@ export default function SelectPageParentDialog() {
                 <DialogContentText>
                     Search for a parent, or select project root. You'll be able to move the page later.
                 </DialogContentText>
-                <Autocomplete
-                    onChange={(event, newValue = null) => {
-                        console.log(newValue);
-                    }}
-                    options={pages}
-                    getOptionLabel={(option) => option.title}
-                    fullWidth
-                    onInputChange={onInputChange}
-                    renderOption={(props, option) => {
-                        return (
-                            <li {...props} key={option._id}>
-                                {option.title}
-                            </li>
-                        );
-                    }}
-                    sx={{mt: 2}}
-                    renderInput={(params) => <TextField {...params} label="Select a parent"/>}
-                />
+                {!isLoading && (
+                    <Autocomplete
+                        onChange={onSelectionChange}
+                        filterOptions={filterOptions}
+                        options={pages}
+                        isOptionEqualToValue={(option, value) => option._id === value._id}
+                        getOptionLabel={(option) => option.title}
+                        fullWidth
+                        autoSelect
+                        value={selectedPage}
+                        renderOption={(props, option) => {
+                            return (
+                                <li {...props} key={option._id}>
+                                    {option.title}
+                                </li>
+                            );
+                        }}
+                        sx={{mt: 2}}
+                        renderInput={(params) => <TextField {...params} label="Select a parent"/>}
+                    />
+                )}
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleClose}>Subscribe</Button>
+                <Button variant="contained" color="error" onClick={handleClose}>Cancel</Button>
+                <Button variant="contained" onClick={handleClose}>Validate</Button>
             </DialogActions>
         </Dialog>
     )
