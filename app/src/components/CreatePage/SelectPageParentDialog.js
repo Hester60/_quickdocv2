6723ec157002b6e768adaa060e91d73ac2019816
betd-tcarbuccia <thomas.carbuccia@businessdecision.com>
2022-06-15 09:42:54 +0200

    Adding form error text

--- app/src/components/CreatePage/SelectPageParentDialog.js
+++ app/src/components/CreatePage/SelectPageParentDialog.js
@@ -3,21 +3,21 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
 import api from '../../api';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import PageAutocompleter from "../PageAutocompleter/PageAutocompleter";
-import {TextField} from "@mui/material";
-import {useFormik} from "formik";
+import { TextField } from "@mui/material";
+import { useFormik } from "formik";
 import * as Yup from 'yup';
-import {validatePageParent, validatePageTitle} from "../../formValidations/pageValidation";
+import { validatePageParent, validatePageTitle } from "../../formValidations/pageValidation";
 
-export default function SelectPageParentDialog({open, handleClose, submitForm}) {
+export default function SelectPageParentDialog({ open, handleClose, submitForm }) {
     const [isLoading, setIsLoading] = useState(true);
     const [pages, setPages] = useState([]);
     const currentProject = useSelector(state => state.currentProject.item);
-    const ROOT_SELECTION = {_id: '*', title: 'Project Root (No Parent)'};
+    const ROOT_SELECTION = { _id: '*', title: 'Project Root (No Parent)' };
 
     // TODO : Don't fetch children !! (API EVOLUTION)
     useEffect(() => {
@@ -59,16 +59,17 @@ export default function SelectPageParentDialog({open, handleClose, submitForm}) 
                         name="title"
                         label="Set a title"
                         type="text"
-                        sx={{mt: 2, mb: 3}}
+                        sx={{ mt: 2, mb: 3 }}
                         onChange={formik.handleChange}
                         value={formik.values.title}
+                        error={(formik.touched.title && formik.errors.title ? true : false)}
+                        helperText={formik.touched.title && formik.errors.title ? (
+                            formik.errors.title
+                        ) : null}
                     />
-                    {formik.touched.title && formik.errors.title ? (
-                        <div>{formik.errors.title}</div>
-                    ) : null}
                     {!isLoading && (
                         <PageAutocompleter pages={pages}
-                                           rootSelection={ROOT_SELECTION} formik={formik}/>
+                            rootSelection={ROOT_SELECTION} formik={formik} />
                     )}
                 </DialogContent>
                 <DialogActions>
