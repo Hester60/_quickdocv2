6c115a95a3c62a6672ae2d0b4b85fb23bdf452c5
Hester60 <thomascarbuccia@hotmail.fr>
2022-06-17 15:39:20 +0200

    Set remove page red

--- app/src/components/Navigation/Menu/PageMenu/RemovePageMenuItem.js
+++ app/src/components/Navigation/Menu/PageMenu/RemovePageMenuItem.js
@@ -1,14 +1,14 @@
-import { MenuItem } from "@mui/material";
+import { MenuItem, Typography } from "@mui/material";
 import { useState } from "react";
 import RemovePageDialog from "../../../RemovePage/RemovePageDialog";
 
 export default function ({page}) {
-   
+
     const [open, setOpen] = useState(false);
 
     return (
         <>
-            <MenuItem onClick={() => {setOpen(true);}}>Remove page</MenuItem>
+            <MenuItem onClick={() => {setOpen(true);}}><Typography color="error">Remove page</Typography></MenuItem>
             <RemovePageDialog page={page} open={open} setOpen={setOpen}/>
         </>
     )
