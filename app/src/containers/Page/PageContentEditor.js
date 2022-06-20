9a6807863fd21e92b49fa7d7b06efa6c7b710232
betd-tcarbuccia <thomas.carbuccia@businessdecision.com>
2022-06-20 12:38:21 +0200

    Removing console log

--- app/src/containers/Page/PageContentEditor.js
+++ app/src/containers/Page/PageContentEditor.js
@@ -30,7 +30,6 @@ export default function PageContentEditor({ formik }) {
                         })
                         .catch(error => {
                             reject("Upload failed");
-                            console.error("Error:", error);
                         });
                 });
             },
