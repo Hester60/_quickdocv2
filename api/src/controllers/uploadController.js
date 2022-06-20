af6e244361e3dbe07857103d37f0af33eccbde63
betd-tcarbuccia <thomas.carbuccia@businessdecision.com>
2022-06-20 13:25:35 +0200

    fixing code block css

--- api/src/controllers/uploadController.js
+++ api/src/controllers/uploadController.js
@@ -1,6 +1,5 @@
 const asyncWrapper = require('../middlewares/asyncWrapper');
 
-
 module.exports.upload = asyncWrapper(async (req, res, next) => {
     return res.status(200).json({
         filename: req.file.filename
