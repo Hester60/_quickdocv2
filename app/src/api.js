b5bf8d75a3aa074eaf8e8419ae9506926dae41b1
Hester60 <thomascarbuccia@hotmail.fr>
2022-06-24 12:59:40 +0200

    Fixing app api url in prod

--- app/src/api.js
+++ app/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = `http://${process.env.NODE_ENV === 'production' ? 'api.' : ''}${window.location.hostname}:${process.env.REACT_APP_API_PORT}${process.env.REACT_APP_API_BASE_PATH}/`;
+export const API_BASE_URL = `http://${process.env.NODE_ENV === 'production' ? 'api.' : ''}${window.location.hostname}${process.env.NODE_ENV === 'production' ? '' : `:${process.env.REACT_APP_API_PORT}`}${process.env.REACT_APP_API_BASE_PATH}/`;
 const instance = axios.create({
     baseURL: API_BASE_URL,
     timeout: 3500,
